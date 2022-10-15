27b3ea9269911e16f556fd0b06a127df13a6a3d8
Keshri47 <115873022+Keshri47@users.noreply.github.com>
2022-10-15 16:22:53 +0530

    Create Shellsprt.py

---
+++ Shellsprt.py
@@ -0,0 +1,49 @@
+def shellsort(arr): 
+
+    n = len(arr) 
+
+    gap = n//2
+
+    while gap > 0: 
+
+        for i in range(gap,n): 
+
+            temp = arr[i] 
+
+            j = i 
+
+            while  j >= gap and arr[j-gap] >temp: 
+
+                arr[j] = arr[j-gap] 
+
+                j -= gap 
+
+            arr[j] = temp 
+
+        gap //= 2
+
+    return arr 
+
+  
+
+arr = [ 12, 34, 54, 2, 3] 
+
+n = len(arr) 
+
+print ("Array before sorting:") 
+
+for i in range(n): 
+
+    print(arr[i]) 
+
+  
+
+arr = shellsort(arr) 
+
+  
+
+print ("\nArray after sorting:") 
+
+for i in range(n): 
+
+    print(arr[i])
